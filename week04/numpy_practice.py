146057eaa8e040b76e30912a7afda98169e44e73
mang8099 <mang8099@gmail.com>
2026-09-08 20:20:41 +0900

    Numpy basics v0.1

---
+++ week04/numpy_practice.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#numpy 리스트 형성-가장 큰 타입으로 통일
+np_array = np.array([3,'2',1.7])
+print(np_array, type(np_array))
+
+#파이썬 리스트 형성-서로다른 타입이 들어감
+list_array = [3,'2',1.7]
+print(list_array, type(list_array))
