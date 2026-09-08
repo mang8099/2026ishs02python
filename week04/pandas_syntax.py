0c8f1fe62fa5ff90332eedd5381033c6df9aa352
mang8099 <mang8099@gmail.com>
2026-09-08 21:43:04 +0900

    Pandas basics v0.2

--- week04/pandas_syntax.py
+++ week04/pandas_syntax.py
@@ -1,8 +1,17 @@
 import numpy as np
 import pandas as pd
 
-array = np.array([[4,7,10], [5,8,11], [6,9,12]])
-df = pd.DataFrame(array, columns = ['a', 'b', 'c'], index = [1,2,3])
+array = np.array([[86,90,64,97], [29,100,76,96], [112,32,142,95]])
+df = pd.DataFrame(array, columns = ['a', 'b', 'c','d'], index = [1,2,3])
 print(df)
-print(df.melt().rename(columns={'variable':'var', 'value':'val'}).query('val <= 7').sort_values(by=['val'], ascending=False).head(3))
+print(df.melt().rename(columns={'variable':'var', 'value':'val'}).query('val <= 50').sort_values(by=['val'], ascending=False).head(3))
 #print(df.sort_values('mpg'))-- #seaborn
+df1 = pd.DataFrame(array, columns = ['Kor', 'Eng', 'Math', 'Cs'], index = [1,2,3])
+print(df1)
+print(df1.iloc[1:, 2:])
+print(df1.iloc[:, [1,3]])
+print(df1.iloc[:, [0,1]])
+print(df1.loc[:, ['Math', 'Cs']])
+print(df1.loc[2:3, 'Math':'Cs'])
+print(df1.iat[2,1])
+print(df1.at[3,'Eng'])
